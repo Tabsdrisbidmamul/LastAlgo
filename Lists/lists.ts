9297d174fc6cf7636bd900d6a64c370cee7e33d1
Idris Khan <ricepoo123@gmail.com>
2023-06-30 16:58:01 +0100

    looking at buffers

--- Lists/lists.ts
+++ Lists/lists.ts
@@ -1,2 +1,19 @@
-const a: string[] = [];
+const a = new ArrayBuffer(6);
+console.log('a ', a);
+
+const a8 = new Uint8Array(a);
+
+console.log('a8 ', a8);
+
+a8[0] = 45;
+a8[2] = 45;
+
+console.log('a ', a);
+
+const a16 = new Uint16Array(a);
+
+console.log('a16 ', a16);
+
+a16[2] = 0x4545;
+
 console.log('a ', a);
